bfd0750fb069b5eea3b9680e83730d8273427d2d
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-07 21:57:25 +0000

    alpha web update ...

---
+++ src/test/java/com/berexia/alphaweb/AlphaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berexia.alphaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
